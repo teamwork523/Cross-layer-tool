c6408edb1295fc41e42724e6e742c6e974716179
Snooperlock <teamworkucla@hotmail.com>
2013-02-03 22:01:30 -0500

    include a seperate class

--- const.py
+++ const.py
@@ -5,8 +5,8 @@ MONTH_MAP = {"jan": 1, "feb": 2, "mar": 3, "apr": 4, \
              "sep": 9, "oct": 10, "nov": 11, "dec": 12}
 
 # Map between log id with log entry
-PROTOCOL_ID = "0x11EB"
-RRC_ID = "0x4125"
+PROTOCOL_ID = int("0x11EB", 16)
+RRC_ID = int("0x4125", 16)
 LOGTYPE_MAP = {PROTOCOL_ID: "Protocol Services Data",
                RRC_ID: "WCDMA RRC States"}
 
@@ -17,3 +17,8 @@ PCH_ID = 4
 RRC_MAP = {FACH_ID: "CELL_FACH",
            DCH_ID: "CELL_DCH",
            PCH_ID: "CELL_PCH"}
+
+# transport layer protocol map
+IP_ID = int("0x01", 16) # QCAT protocol id
+TLP_MAP = {6:"TCP",
+           17: "UDP"}
--- qxdm_mapper.py
+++ qxdm_mapper.py
@@ -7,68 +7,10 @@ This program analyze the Data Set generated from QXDM filtered log file
 Then map the packets from PCAP with the RRC states in the log
 """
 
-import sys, re
-import calendar
+import sys
 import const
-from datetime import datetime
+import QCATEntry as qe
 from optparse import OptionParser
-
-class QCATEntry:
-    """
-    Each Entry should have three part:
-    1. Title: timestamp, log_id (i.e. 0x4125), log_type (i.e. WCDMA RRC States)
-    2. Detail: specific type information
-    3. Hex_dump: detailed hex informaiton
-    """
-    def __init__(self, title, detail, hex_dump):
-        self.title = title
-        self.detail = detail
-        self.hex_dump = hex_dump
-        # timestamp: [unix_timestamp, milliseconds]
-        self.timestamp = []
-        # log id
-        self.logID = None
-        # RRC id
-        self.rrcID = None
-        # order matters
-        self.__procTitle()
-        self.__procDetail()
-        #self.__procHexDump()
-
-    def __procTitle(self):
-        print "Process Titile"
-        if self.title != "":
-            tList = self.title.split()
-            # TODO: Support time difference
-            # Parse the timestamp, only support UTC right now
-            year = (int)(tList[0])
-            month = (int)(const.MONTH_MAP[tList[1].lower()])
-            day = (int)(tList[2])
-            [secsList, millisec] = tList[3].split('.')
-            [hour, minutes, sec] = secsList.split(':')
-            dt = datetime(year, month, day, (int)(hour), (int)(minutes), (int)(sec))
-            unixTime = calendar.timegm(dt.utctimetuple())
-            self.timestamp = [unixTime, (int)(millisec)]
-            print self.timestamp
-            # Parse the log id
-            self.logID = tList[5]
-            print self.logID
-        else:
-            self.title = None
-
-    def __procDetail(self):
-        if self.detail[0].find("not supported") != -1:
-            self.detail = None
-        else:
-            print "Process detail"
-            if self.logID == const.RRC_ID:
-                rrclist = self.detail[0].split()
-                # extract int from parentheses
-                self.rrcID = (int)(re.findall("\d+",rrclist[-1])[0])
-                print "Id:%s, RRC_state:%s" % (self.rrcID, const.RRC_MAP[self.rrcID])
-
-    def __procHexDump(self):
-        print "c"
 
 def init_optParser():
     optParser = OptionParser(usage="./%prog [-l, --log] QCAT_LOG_PATH")
@@ -97,11 +39,8 @@ def readQCATLog(inQCATLogFile):
             countNewline += 1
             if countNewline > 1 and countNewline % 2 == 1:
                 print "*"*40
-                print countNewline
-                print titleAndDetail
-                print hexDump
-                QCATEntries.append(QCATEntry(titleAndDetail[0], 
-                                             titleAndDetail[1:], hexDump))
+                QCATEntries.append(qe.QCATEntry(titleAndDetail[0], 
+                                                titleAndDetail[1:], hexDump))
                 titleAndDetail = []
                 hexDump = []
         else:
@@ -114,6 +53,15 @@ def readQCATLog(inQCATLogFile):
 
     return QCATEntries
 
+def assignRRCState(entries):
+    mostRecentRRCID = None
+    for entry in entries:
+        if entry.logID == const.RRC_ID:
+            mostRecentRRCID = entry.rrcID
+        elif entry.logID == const.PROTOCOL_ID:
+            if entry.rrcID == None and mostRecentRRCID != None:
+                entry.rrcID = mostRecentRRCID
+
 def main():
     # read lines from input file
     optParser = init_optParser()
@@ -123,6 +71,12 @@ def main():
         optParser.error("-l, --log: Empty filename")
 
     QCATEntries = readQCATLog(options.inQCATLogFile)
+    print len(QCATEntries)
+    assignRRCState(QCATEntries)
+    for i in QCATEntries:
+        if i.rrcID != None and i.ip["tlp_id"] != None:
+            print "RRC: %d, Protocol: %d" % (i.rrcID, i.ip["tlp_id"])
+
 
 if __name__ == "__main__":
     main()
