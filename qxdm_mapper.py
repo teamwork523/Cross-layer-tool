07263c501424df3c094837f34490734fc29f8e6f
Snooperlock <teamworkucla@hotmail.com>
2013-02-04 21:21:26 -0500

    make support multiple entries for the same timestamp

--- qxdm_mapper.py
+++ qxdm_mapper.py
@@ -43,7 +43,9 @@ def readQCATLog(inQCATLogFile):
                 entry = qe.QCATEntry(titleAndDetail[0], titleAndDetail[1:], hexDump)
                 key = entry.timestamp[0]*1000 + entry.timestamp[1]
                 if QCATEntries.has_key(key) == False:
-                    QCATEntries[key] = entry
+                    QCATEntries[key] = [entry]
+                else:
+                    QCATEntries[key].append(entry)
                 titleAndDetail = []
                 hexDump = []
         else:
@@ -75,7 +77,9 @@ def readPCAPResultFile(pcapResultFile):
                 packet = pp.PCAPPacket(timestamp, millisec, payload)
                 key = packet.timestamp[0] * 1000 + packet.timestamp[1]
                 if PCAPPackets.has_key(key) == False:
-                    PCAPPackets[key] = packet
+                    PCAPPackets[key] = [packet]
+                else:
+                    PCAPPackets[key].append(packet)
         if curIndex == 1:
             [timestamp, millisec] = line.split()
         if curIndex == 2:
@@ -84,32 +88,50 @@ def readPCAPResultFile(pcapResultFile):
 
 def assignRRCState(entries):
     mostRecentRRCID = None
-    for entry in entries.values():
-        if entry.logID == const.RRC_ID:
-            mostRecentRRCID = entry.rrcID
-        elif entry.logID == const.PROTOCOL_ID:
-            if entry.rrcID == None and mostRecentRRCID != None:
-                entry.rrcID = mostRecentRRCID
+    for entryList in entries.values():
+        for entry in entryList:
+            if entry.logID == const.RRC_ID:
+                mostRecentRRCID = entry.rrcID
+            elif entry.logID == const.PROTOCOL_ID:
+                if entry.rrcID == None and mostRecentRRCID != None:
+                    entry.rrcID = mostRecentRRCID
 
 def mapPCAPwithQCAT(p, q):
-    countMap = 0
+    countMap = {}
+    QCATFast = 0
+    QCATSame = 0
+    QCATSlow = 0
+    countMap[const.FACH_ID] = 0
+    countMap[const.DCH_ID] = 0
+    countMap[const.PCH_ID] = 0
+    total = 0
     for pktKey in sorted(p.keys()):
-        ts = datetime.fromtimestamp(p[pktKey].timestamp[0]).strftime('%Y-%m-%d %H:%M:%S')
+        ts = datetime.fromtimestamp(p[pktKey][0].timestamp[0]).strftime('%Y-%m-%d %H:%M:%S')
+        # Only map the first entry in the mapped list for right now
         if q.has_key(pktKey-const.TS_DELTA):
-            countMap += 1
-            print "%d- %s %s %s" % (countMap, ts, const.TLP_MAP[q[pktKey-const.TS_DELTA].ip["tlp_id"]], \
-                                    const.RRC_MAP[q[pktKey-const.TS_DELTA].rrcID])
+            QCATSlow += 1
+            total = QCATSlow + QCATSame + QCATFast
+            print "%d(QCAT slow)- %s %s %s" % (total, ts, const.TLP_MAP[q[pktKey-const.TS_DELTA][0].ip["tlp_id"]], \
+                                    const.RRC_MAP[q[pktKey-const.TS_DELTA][0].rrcID])
+            countMap[q[pktKey-const.TS_DELTA][0].rrcID] += 1
             continue
         elif q.has_key(pktKey):
-            countMap += 1
-            print "%d: %s %s %s" % (countMap, ts, const.TLP_MAP[q[pktKey].ip["tlp_id"]], \
-                                    const.RRC_MAP[q[pktKey].rrcID])
+            QCATSame += 1
+            total = QCATSlow + QCATSame + QCATFast
+            print "%d(QCAT same): %s %s %s" % (total, ts, const.TLP_MAP[q[pktKey][0].ip["tlp_id"]], \
+                                    const.RRC_MAP[q[pktKey][0].rrcID])
+            countMap[q[pktKey][0].rrcID] += 1
             continue
         elif q.has_key(pktKey+const.TS_DELTA):
-            countMap += 1
-            print "%d+ %s %s %s" % (countMap, ts, const.TLP_MAP[q[pktKey+const.TS_DELTA].ip["tlp_id"]], \
-                                    const.RRC_MAP[q[pktKey+const.TS_DELTA].rrcID])
+            QCATFast += 1
+            total = QCATSlow + QCATSame + QCATFast
+            print "%d(QCAT fast)+ %s %s %s" % (total, ts, const.TLP_MAP[q[pktKey+const.TS_DELTA][0].ip["tlp_id"]], \
+                                    const.RRC_MAP[q[pktKey+const.TS_DELTA][0].rrcID])
+            countMap[q[pktKey+const.TS_DELTA][0].rrcID] += 1
             continue
+    countMap["fast"] = QCATFast
+    countMap["same"] = QCATSame
+    countMap["slow"] = QCATSlow
     return countMap
 
 def main():
@@ -139,10 +161,16 @@ def main():
     PCAPPackets = readPCAPResultFile(outFile)
 
     countMap = mapPCAPwithQCAT(PCAPPackets, QCATEntries)
-    
+    totalCount = countMap["fast"] + countMap["slow"] + countMap["same"]
     print "*"*40
     print "In total %d packets"%(len(PCAPPackets))
-    print "Mapping rate is %f"%((float)(countMap)/(float)(len(PCAPPackets)))
+    print "Mapping rate is %f"%((float)(totalCount)/(float)(len(PCAPPackets)))
+    print "QCAT ahead rate is %f"%((float)(countMap["fast"])/(float)(len(PCAPPackets)))
+    print "QCAT same rate is %f"%((float)(countMap["same"])/(float)(len(PCAPPackets)))
+    print "QCAT slow rate is %f"%((float)(countMap["slow"])/(float)(len(PCAPPackets)))
+    print "DCH state rate is %f"%((float)(countMap[const.DCH_ID])/(float)(len(PCAPPackets)))
+    print "FACH state rate is %f"%((float)(countMap[const.FACH_ID])/(float)(len(PCAPPackets)))
+    print "PCH state rate is %f"%((float)(countMap[const.PCH_ID])/(float)(len(PCAPPackets)))
 
 if __name__ == "__main__":
     main()
